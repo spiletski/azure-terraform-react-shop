8dbdb522359dc664421f850c349a4e37a47ecec0
spiletski <stanislau@mail.ru>
2024-03-27 18:02:10 +0100

    task-2: serving spa in azure

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,3 +19,18 @@ resource "azurerm_resource_group" "front_end_rg" {
   name     = "rg-frontend-sand-ne-001"
   location = "northeurope"
 }
+
+
+resource "azurerm_storage_account" "front_end_storage_account" {
+  name     = "spiletskistoragene001"
+  location = "northeurope"
+
+  account_replication_type = "LRS"
+  account_tier             = "Standard"
+  account_kind             = "StorageV2"
+  resource_group_name      = azurerm_resource_group.front_end_rg.name
+
+  static_website {
+    index_document = "index.html"
+  }
+}
