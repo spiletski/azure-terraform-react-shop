d3162a252728364d4a057a09c668bc2fcb3364ab
spiletski <stanislau@mail.ru>
2024-03-27 22:26:13 +0100

    task-2: update scripts

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,26 +9,23 @@ terraform {
   required_version = ">= 1.1.0"
 }
 
-
 provider "azurerm" {
   features {}
 }
 
-
-resource "azurerm_resource_group" "front_end_rg" {
-  name     = "rg-frontend-sand-ne-001"
-  location = "northeurope"
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
 }
 
-
 resource "azurerm_storage_account" "front_end_storage_account" {
-  name     = "spiletskistoragene001"
+  name     = "spiletskistorageacc02"
   location = "northeurope"
 
   account_replication_type = "LRS"
   account_tier             = "Standard"
   account_kind             = "StorageV2"
-  resource_group_name      = azurerm_resource_group.front_end_rg.name
+  resource_group_name      = azurerm_resource_group.rg.name
 
   static_website {
     index_document = "index.html"
