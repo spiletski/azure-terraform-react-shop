435c8ff137942cad714713b396e0af512407ca24
spiletski <31621040+spiletski@users.noreply.github.com>
2024-03-29 16:13:25 +0100

    Task 2: Serving SPA in Azure (#2)
    * task-2: serving spa in azure
    
    * task-2: update scripts

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,13 +9,25 @@ terraform {
   required_version = ">= 1.1.0"
 }
 
-
 provider "azurerm" {
   features {}
 }
 
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
+}
 
-resource "azurerm_resource_group" "front_end_rg" {
-  name     = "rg-frontend-sand-ne-001"
+resource "azurerm_storage_account" "front_end_storage_account" {
+  name     = "spiletskistorageacc02"
   location = "northeurope"
+
+  account_replication_type = "LRS"
+  account_tier             = "Standard"
+  account_kind             = "StorageV2"
+  resource_group_name      = azurerm_resource_group.rg.name
+
+  static_website {
+    index_document = "index.html"
+  }
 }
